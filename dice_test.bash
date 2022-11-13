e3f3adc88d8ebceec19667c910d99d242358c6e0
18C1054_S_K <s18c1054uc@s.chibakoudai.jp>
2022-11-13 18:08:39 +0900

    update dice_test.bash

--- dice_test.bash
+++ dice_test.bash
@@ -4,8 +4,8 @@
 # SPDX-FileCopyrightIdentifer: MIT License
 
 ng () {
-	echo "NG in line $1"
-	ret=1
+  echo "NG in line $1"
+  ret=1
 }
 
 ret=0
@@ -24,7 +24,33 @@ out=$(./dice.py ${arg1})
 arg1=10
 arg2=5
 out=$(./dice.py ${arg1} ${arg2})
-[ "${out}" = "5 3 6 3 6 6 6 5 1 4" ] || ng ${LINENO}
+#[ "${out}" = "5 3 6 3 6 6 6 5 1 4" ] || ng ${LINENO}
+
+diff <(cat expected_output.txt) <(echo "${out}")
+[ $? = 0 ] || ng ${LINENO}
+
+<< comment
+ng_output (){
+  echo "different output @ line $1"
+  ret=1
+}
+
+#echo ${out}
+IFS=$'\n'
+dices_lines=(${dices})
+out_lines=(${out})
+echo ${dices_lines[1]}
+echo ${out_lines[1]}
+[ ${out_lines[1]} = ${dices_lines[1]} ] && echo "match!!" || echo "different..."
+if [ ${#out_lines[*]} -eq ${#dices_lines[*]} ] ; then
+  n=${#out_lines[*]}
+  for ((i=0; i < ${n}; i++)); do
+    [ ${out_lines[${i}]} = ${dices_lines[${i}]} ] || ng_output ${i}+1
+  done
+else
+  ng ${LINENO}
+fi
+comment
 
 exit ${ret}
 
